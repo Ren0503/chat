e36b4b8611dad7b6a8e67e9b8cc4490ffd5089a5
Ren0503 <illuminati0440@gmail.com>
2021-12-06 13:34:20 +0700

    Add index

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,8 @@
-import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 import connectDB from './config/db';
 import { notFound, errorHandler } from './middleware/errorMiddleware';
@@ -15,11 +16,59 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('combined'));
 }
 
 app.use(express.json());
 app.use('/api/chat', chatRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/message', messageRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    pingTimeout: 60000,
+    cors: {
+        origin: 'http://localhost:3000',
+        // credentials: true,
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('Connected to socket.io');
+    socket.on('setup', (userData) => {
+        socket.join(userData._id);
+        socket.emit('connected');
+    });
+
+    socket.on('join chat', (room) => {
+        socket.join(room);
+        console.log('User Joined Room: ' + room);
+    });
+    socket.on('typing', (room) => socket.in(room).emit('typing'));
+    socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
+
+    socket.on('new message', (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+
+        if (!chat.users) return console.log('chat.users not defined');
+
+        chat.users.forEach((user) => {
+            if (user._id == newMessageReceived.sender._id) return;
+
+            socket.in(user._id).emit('message received', newMessageReceived);
+        });
+    });
+
+    socket.off('setup', () => {
+        console.log('USER DISCONNECTED');
+        socket.leave(userData._id);
+    });
+});
+
+httpServer.listen(PORT);
