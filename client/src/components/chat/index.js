b5d6767d56b3b49f5012d8b2f688e2f258fcf811
Ren0503 <illuminati0440@gmail.com>
2021-12-08 09:58:49 +0700

    Add chats components

---
+++ client/src/components/chat/index.js
@@ -0,0 +1,4 @@
+export { default as ChatBox } from './ChatBox';
+export { default as MyChat } from './MyChat';
+export { default as Scrollable } from './Scrollable';
+export { default as SingleChat } from './SingleChat';
