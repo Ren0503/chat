963208633f998b7efe33acbb968ec84b369ff9ad
Ren0503 <illuminati0440@gmail.com>
2021-12-08 09:21:44 +0700

    Add core

---
+++ client/src/components/core/modals/index.js
@@ -0,0 +1,3 @@
+export { default as ProfileModal } from './ProfileModal';
+export { default as GroupChatModal } from './GroupChatModal';
+export { default as UpdateGroupChatModal } from './UpdateGroupChatModal';
